c27c69e531d9d33eeb7804f29a5a1307989aa9ef
bakfietss <webshophethuisdiertje@gmail.com>
2026-01-27 17:03:26 +0100

    project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Shake It Up</h1>
+      <p>Cocktail applicatie</p>
+    </div>
+  )
+}
+
+export default App
