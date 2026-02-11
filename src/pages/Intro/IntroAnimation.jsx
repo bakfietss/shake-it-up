17bc5a22e578853479529f997da77ada70d0a805
bakfietss <webshophethuisdiertje@gmail.com>
2026-02-11 16:21:55 +0100

    cleanup ongebruikte imports en variabelen

--- src/pages/Intro/IntroAnimation.jsx
+++ src/pages/Intro/IntroAnimation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import "./IntroAnimation.scss";
 
 import { pathLengths, pathData, INTRO_TIMELINE, scheduleTimers } from "./introHelpers";
