66de07eb89053291009b941f13284dd9e978836f
bakfietss <webshophethuisdiertje@gmail.com>
2026-02-12 06:50:52 +0100

    favoritebutton component

--- src/components/CocktailCard/CocktailCard.jsx
+++ src/components/CocktailCard/CocktailCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useToast } from "../../context/ToastContext";
 import { useFavorites } from "../../context/FavoritesContext";
+import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import "./CocktailCard.scss";
 
 function CocktailCard({ idDrink, strDrink, strDrinkThumb, strCategory, strAlcoholic, strGlass }) {
@@ -25,11 +26,7 @@ function CocktailCard({ idDrink, strDrink, strDrinkThumb, strCategory, strAlcoho
     <Link to={`/cocktail/${idDrink}`} className="cocktail-card">
       <div className="card-image-wrapper">
         <img src={strDrinkThumb} alt={strDrink} />
-        <button className={`favorite-button ${isFav ? "is-favorite" : ""}`} onClick={handleFavoriteClick}>
-          <svg viewBox="0 0 24 24">
-            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-          </svg>
-        </button>
+        <FavoriteButton className="favorite-button" isFavorite={isFav} onClick={handleFavoriteClick} />
       </div>
       <div className="card-info">
         <h3>{strDrink}</h3>
--- src/pages/DetailsCocktail/Details.jsx
+++ src/pages/DetailsCocktail/Details.jsx
@@ -5,6 +5,7 @@ import api from "../../helpers/api";
 import { AuthContext } from "../../context/AuthContext";
 import { useFavorites } from "../../context/FavoritesContext";
 import { useToast } from "../../context/ToastContext";
+import FavoriteButton from "../../components/FavoriteButton/FavoriteButton";
 import "./Details.scss";
 
 function Details() {
@@ -99,11 +100,7 @@ function Details() {
         <div className="details-left">
           <div className="cocktail-title-row">
             <h1 className="cocktail-name">{cocktail.strDrink}</h1>
-            <button className={`favorite-heart ${isFav ? "is-favorite" : ""}`} onClick={handleFavoriteClick}>
-              <svg viewBox="0 0 24 24">
-                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-              </svg>
-            </button>
+            <FavoriteButton className="favorite-heart" isFavorite={isFav} onClick={handleFavoriteClick} />
           </div>
 
           <div className="cocktail-image-wrapper">
